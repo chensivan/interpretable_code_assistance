ac3ef7f084ad64a20ce367ca4ee1a69b263a61cf
LeshiYang <yanglesh666@gmail.com>
2022-05-16 12:26:44 -0400

    fix bug: transform.translate get updated with each drag event

--- vs_code_extension/helloworld/CodePanel.js
+++ vs_code_extension/helloworld/CodePanel.js
@@ -139,9 +139,8 @@ class CodePanel {
 
   // Generate comment for printing to the editor
   generateComment(position, info){
-    return "// Move the <"+ info + "> from (" + position[0] + "," + position[1] + ") to (" + position[2] + "," + position[3] + ").";
+    return "<!-- Move the <"+ info + "> from (" + position[0] + "," + position[1] + ") to (" + position[2] + "," + position[3] + ").-->";
   };
-
   //Print comment to the top of editor
   _printCommentToEditor(comment){
     const file = CodePanel.filePath;
@@ -179,7 +178,6 @@ class CodePanel {
     const nonce = getNonce();
     
     CodePanel.filePath = vscode.window.activeTextEditor.document.fileName;
-    this._printCommentToEditor('//Start generating code');
     
     //Read the file
     const file = fs.readFileSync(CodePanel.filePath, "utf8");
@@ -218,83 +216,6 @@ class CodePanel {
         })
       }
     });
-    var svgMoving = false;
-    // set drag event to svg element
-    // var svgs = document.querySelectorAll("svg");
-    // if (svgs.length > 0){
-    //   var svg = svgs[0];
-    //   svg.setAttribute("onload", "makeSvgDraggable(event)");
-
-    //   var svgMoving = false; // flag: true indicating dragging on svg element
-  
-    //   function makeSvgDraggable(event){
-    //     var svg = event.target;
-    //     svg.addEventListener('mousedown', startDrag);
-  
-    //     var selectedElement, offset, transform;
-    //     function startDrag(evt){
-    //       evt.preventDefault();
-    //       selectedElement = evt.target;
-    //       /*set dragging unit to 'g'*/
-    //       while (selectedElement.tagName !== "g" && selectedElement){
-    //         selectedElement = selectedElement.parentNode;
-    //       };
-    //       if (selectedElement){
-    //         if (selectedElement.tagName === "g" && dragEnable){
-    //           svgMoving = true;
-    //           selectedElement.addEventListener('mousemove', drag);
-    //           selectedElement.addEventListener('mouseup', endDrag);
-    //           selectedElement.addEventListener('mouseleave', endDrag);
-    
-    //           offset = getMousePosition(evt);
-    //           //Get all the transforms currently on this element
-    //           var transforms = selectedElement.transform.baseVal;
-    //           if (transforms.length === 0 ||
-    //             transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
-    //               var translate = svg.createSVGTransform();
-    //               translate.setTranslate(0, 0);
-    //               selectedElement.transform.baseVal.insertItemBefore(translate, 0);
-      
-    //             };
-    //             transform = transforms.getItem(0);
-    //             offset.x -= transform.matrix.e;
-    //             offset.y -= transform.matrix.f;
-    //         };
-    //       };
-  
-          
-    //     };
-      
-    //     function drag(evt){
-    //       if (selectedElement && svgMoving) {
-    //         evt.preventDefault();
-    //         var coord = getMousePosition(evt);
-    //         transform.setTranslate(coord.x - offset.x, coord.y - offset.y);
-    //       }
-    //     };
-    //     function endDrag(evt){
-    //       if (svgMoving) {
-    //         var coord = getMousePosition(evt);
-    //         var selector = defineSelector(selectedElement);
-    //         vscode.postMessage({
-    //           type: 'passPosition',
-    //           value: [offset.x, offset.y, coord.x, coord.y],
-    //           info: selector,
-    //         })
-    //         selectedElement = null;
-    //         svgMoving = false;
-    //       }
-    //     };
-    //     //Get relevant coordinates of SVG space
-    //     function getMousePosition(evt) {
-    //       var CTM = svg.getScreenCTM();
-    //       return {
-    //         x: (evt.clientX - CTM.e) / CTM.a,
-    //         y: (evt.clientY - CTM.f) / CTM.d
-    //       };
-    //     }
-    //   }
-    // }
 
     function defineSelector(element){
       var selector = element.outerHTML;
@@ -320,14 +241,20 @@ class CodePanel {
     var div;
 
     function dragStart(e) {
-      // if not svg moving then
-      if (!svgMoving && dragEnable){
+      if (dragEnable){
         div = e.target;
 
-        // set minimum dragging unit to 'DIV"
+        // set minimum dragging unit(i.e to 'DIV')
         // while (div.tagName !== "DIV"){
         //   div = div.parentNode;
         // };
+        
+        transformValue = window.getComputedStyle(div).transform;
+        if (transformValue){
+          var matrix = new WebKitCSSMatrix(transformValue);
+          translateX = matrix.m41;
+          translateY = matrix.m42;
+        }
 
         div.addEventListener("mousemove", drag);
         div.addEventListener("mouseup", dragEnd);
@@ -338,8 +265,6 @@ class CodePanel {
     }
 
     function drag(e) {
-      // e = e || window.event;
-      // e.preventDefault();
       if (moving) {
         if (lastX&&lastY){
           var pX = e.clientX - lastX;
@@ -367,6 +292,9 @@ class CodePanel {
   
         lastX = null;
         lastY = null;
+
+        translateX = 0;
+        translateY = 0;
       }
     }
     </script>`;
