1ecd990a304ec509603c024d8afac920bfe65518
angely67 <82392932+angely67@users.noreply.github.com>
2022-05-31 15:32:20 -0400

    add mongo logging function

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@
 from flask import Flask, request
 import json
 import nlpcloud
-
+import pymongo
+import json
 
 # Create flask app
 flask_app = Flask(__name__)
 client = nlpcloud.Client("fast-gpt-j", "aba1569138b09087b8a839356381010dd56d6679", gpu=True, lang="en")
+client = pymongo.MongoClient("mongodb+srv://user:user@cluster.menscjh.mongodb.net/?retryWrites=true&w=majority")
+db = client["copilot_plus_plus"]
 
 @flask_app.route("/paraphrase", methods = ["POST"])
 def paraphrase():
@@ -18,6 +21,16 @@ def paraphrase():
 def paraphrase_text(text):
     paraphrased_text = client.paraphrasing(text)
     return paraphrased_text
+
+@flask_app.route("/db/insertLog", methods = ["POST"])
+def test():
+    mycol = db["log"]
+    body = request.json
+
+    mydict = { "event": body["event"], "details": body["details"] }
+
+    x = mycol.insert_one(mydict)
+    return str(x.inserted_id)
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
