307fe702c6d3df4371a0166b42335e652a92233f
angely67 <82392932+angely67@users.noreply.github.com>
2022-06-02 14:59:57 -0400

    add autocomplete

--- vs_code_extension/copilot_plus_plus/media/webview.js
+++ vs_code_extension/copilot_plus_plus/media/webview.js
@@ -62,7 +62,8 @@ document.addEventListener("click", function(event){
   if (event.target.id !== "inputbox" && event.target.parentElement.id !== "inputbox"
   && event.target.id !== "navbar" && event.target.parentElement.id !== "navbar"
   && (event.target.tagName !== "HTML") 
-  && event.target.id !== "chatBotOuter" && event.target.parentElement.id !== "chatBotOuter"){
+  && event.target.id !== "chatBotOuter" && event.target.parentElement.id !== "chatBotOuter"
+  && event.target.id !== "inputbox-event"){
     if (mode == 0){
       createEditBox(event.pageX, event.pageY, event.target);
     }
@@ -307,7 +308,7 @@ function createEditBox(x, y, element){
   closeInputBox();
   let inputbox = createBasicBox(x, y);
   if(element.tagName !== "BODY"){
-    inputbox.innerHTML = "<textarea style='background:white' id='inputbox-input' rows='5'></textarea><button id='inputbox-submit'>Submit</button><button id='inputbox-close'>X</button>";
+    inputbox.innerHTML = "<textarea style='background:white' id='inputbox-input' rows='5' cols='50'></textarea><button id='inputbox-submit'>Submit</button><button id='inputbox-close'>X</button>";
   }
   else{
     inputbox.innerText = "document body";
@@ -360,9 +361,16 @@ function createDeleteBox(x, y, element){
 function createInputBoxJs(x, y, element){
   closeInputBox();
   createInfoBox(x, y, element);
+  let div = document.createElement("div");
+  div.classList.add("autocomplete");
   let event = document.createElement("input");
   event.id = "inputbox-event";
+
+  div.appendChild(event);
+  autocomplete(event, EVENTS);
+
   event.placeholder = "action event, ex. onclick";
+
   
   let name = document.createElement("input");
   name.id = "inputbox-name";
@@ -392,7 +400,7 @@ function createInputBoxJs(x, y, element){
   });
   
   document.querySelector("#inputbox").appendChild(document.createElement("br"));
-  document.querySelector("#inputbox").appendChild(event);
+  document.querySelector("#inputbox").appendChild(div);
   document.querySelector("#inputbox").appendChild(document.createElement("br"));
   document.querySelector("#inputbox").appendChild(name);
   document.querySelector("#inputbox").appendChild(document.createElement("br"));
@@ -560,4 +568,108 @@ function dragEnd(e) {
     
   }
 }
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the country names in the world:*/
+var EVENTS = ["onchange", "onclick", "onmouseover", "onmouseout",
+  "onmousedown", "onmouseup", "onkeydown", "onkeypress", "onkeyup",
+  "onfocus", "onblur", "onload", "onunload", "onabort", "onerror",
+  "onresize", "onscroll", "onselect", "onchange", "onreset"]
+
 
