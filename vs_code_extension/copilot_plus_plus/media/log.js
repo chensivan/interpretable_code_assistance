b2ac98b6a202388c667b1008b390931d20c6c85a
LeshiYang <yanglesh666@gmail.com>
2022-06-03 11:28:17 -0400

    add imported side panel

---
+++ vs_code_extension/copilot_plus_plus/media/log.js
@@ -0,0 +1,93 @@
+var json = [{
+  "_id": {
+    "$oid": "62967590477bb19deca0a608"
+  },
+  "userId": "user1",
+  "event": "insert",
+  "details": "orange absolute position, position at top 61px, left 196px with width 131px and height 27px"
+},{
+  "_id": {
+    "$oid": "629676a3acc62dda22ebc0a7"
+  },
+  "userId": "user1",
+  "event": "insert",
+  "details": "picture of a orange cat absolute position, position at top 58px, left 199px with width 148px and height 55px",
+  "createDate": {
+    "$date": {
+      "$numberLong": "1654013539255"
+    }
+  },
+  "updateDate": {
+    "$date": {
+      "$numberLong": "1654013539255"
+    }
+  }
+},{
+  "_id": {
+    "$oid": "6296c63fe52a1d637422a36c"
+  },
+  "userId": "user1",
+  "event": "change attributes",
+  "details": "<p>This is a paragraph.</p> to <p>This is a paragraph.</p>",
+  "createDate": {
+    "$date": {
+      "$numberLong": "1654033919378"
+    }
+  },
+  "updateDate": {
+    "$date": {
+      "$numberLong": "1654033919378"
+    }
+  }
+},{
+  "_id": {
+    "$oid": "6296c690e52a1d637422a36d"
+  },
+  "userId": "user1",
+  "event": "change attributes",
+  "details": "<p>This is a paragraph.</p> to <p>This is a paragraph.</p>",
+  "createDate": {
+    "$date": {
+      "$numberLong": "1654034000992"
+    }
+  },
+  "updateDate": {
+    "$date": {
+      "$numberLong": "1654034000992"
+    }
+  }
+},{
+  "_id": {
+    "$oid": "6296c693e52a1d637422a36e"
+  },
+  "userId": "user1",
+  "event": "change attributes",
+  "details": "<div>This is another paragraph.</div> to <div>This is another paragraph.</div>",
+  "createDate": {
+    "$date": {
+      "$numberLong": "1654034003536"
+    }
+  },
+  "updateDate": {
+    "$date": {
+      "$numberLong": "1654034003536"
+    }
+  }
+},{
+  "_id": {
+    "$oid": "6296c6a1e52a1d637422a36f"
+  },
+  "userId": "user1",
+  "event": "delete",
+  "details": "<div>This is another paragraph.</div>",
+  "createDate": {
+    "$date": {
+      "$numberLong": "1654034017747"
+    }
+  },
+  "updateDate": {
+    "$date": {
+      "$numberLong": "1654034017747"
+    }
+  }
+}];
