dabbe4f3eb2f75a968f8287507db45a1645daa3c
LeshiYang <yanglesh666@gmail.com>
2022-06-09 12:03:06 -0400

    fix log onclick event

--- vs_code_extension/copilot_plus_plus/media/webview.js
+++ vs_code_extension/copilot_plus_plus/media/webview.js
@@ -143,7 +143,8 @@ document.addEventListener("click", function(event){
   && (event.target.tagName !== "HTML") 
   && event.target.id !== "chatBotOuter" && (!event.target.parentElement ||event.target.parentElement.id !== "chatBotOuter")
   && event.target.id !== "inputbox-event"
-  && !findAncestor(event.target, "sidePanel")){
+  && !findAncestor(event.target, "sidePanel")
+  && event.target.id !== "openbtn"){
     if (mode == 0){
       createEditBox(event.pageX, event.pageY, event.target);
     }
